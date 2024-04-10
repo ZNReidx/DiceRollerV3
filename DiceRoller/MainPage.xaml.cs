0bf725b9f5df028526835e1d4090df3e90aaa964
etechas <etechas@LI08-03>
2024-04-10 15:22:37 -0300

    Add project files.

---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(){
+           InitializeComponent();
+        }
+        private void RandomBtn_Clicked(object sender, EventArgs e){
+            if (int.TryParse(entradaLados.Text, out int numbers)){
+                Random random = new Random();
+            
+                int lados = Convert.ToInt32(entradaLados.Text);
+                numbers = random.Next(1, lados + 1);
+                resultadoLabel.Text = $"Resultado: {numbers}";   
+            }
+            
+        }
+
+       
+    }
+
+}
